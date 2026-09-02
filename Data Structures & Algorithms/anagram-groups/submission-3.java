2231ce62b42cf9f2b6c846c64f5b356ef1f72255
hrithikdsetty <hrithikdsetty@gmail.com>
2026-09-02 15:15:52 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,6 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return Arrays.stream(strs)
+                .collect(Collectors.groupingBy(s->s.chars().sorted().mapToObj(c->String.valueOf((char)c)).collect(Collectors.joining()))).values().stream().toList();
+    }
+}
