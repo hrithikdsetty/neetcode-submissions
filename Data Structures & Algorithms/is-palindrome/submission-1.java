cd66a67111cddddf61631cfe2b12d97569f73a2a
hrithikdsetty <hrithikdsetty@gmail.com>
2026-09-05 15:51:52 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String temp = s.chars().filter(c-> (c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9')).mapToObj(c->String.valueOf((char) c)).map(c->c.toUpperCase()).collect(Collectors.joining());
+
+        int i = 0;
+
+        int j = temp.length()-1;
+
+        while(i<=j){
+            if(temp.charAt(i)!=temp.charAt(j)) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
